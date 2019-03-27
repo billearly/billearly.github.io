bd57da4dcfa8d06b047e4df42b879f916f601954
Bill Early <earlybilly@gmail.com>
2019-03-26 21:26:53 -0400

    Gatsby site

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  background-color: red;
+`;
+
+export default () => {
+  return (
+    <StyledDiv>
+      <p>Hey</p>
+    </StyledDiv>
+  );
+}
